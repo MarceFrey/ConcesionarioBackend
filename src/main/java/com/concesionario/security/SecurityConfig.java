b755dbb9707e54a00ad32a928e13c93a02ff7f2e
MarceFrey <you@example.com>
2025-07-18 16:44:41 -0300

    Se eliminan bloqueos para get de vehiculos

--- src/main/java/com/concesionario/security/SecurityConfig.java
+++ src/main/java/com/concesionario/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class SecurityConfig {
@@ -18,6 +19,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/vehiculos").permitAll() // <- LIBRE para listar
                         .requestMatchers("/api/vehiculos/subir").hasAuthority("ADMIN")
                         .anyRequest().authenticated()
                 )
